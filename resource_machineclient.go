ed4cca8fd802d8756e10cdb79005dfb0e4b3954c
Joachim Halvorsen Seilen <joachim.halvorsen@elvia.no>
2023-01-10 07:44:18 +0100

    Using Context

--- resource_machineclient.go
+++ resource_machineclient.go
@@ -1,29 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"strconv"
 
 	"github.com/3lvia/terraform-provider-elvid/elvidapiclient"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceMachineClient() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceMachineClientCreate,
-		Read:   resourceMachineClientRead,
-		Delete: resourceMachineClientDelete,
-		Update: resourceMachineClientUpdate,
-		Importer: &schema.ResourceImporter{
-			State: func(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
-				providerInput := m.(*ElvidProviderInput)
-				d.SetId(d.Id())
-				d.Set("resource_taint_version", "1")
-				d.Set("token_endpoint", providerInput.ElvIDAuthority+"/connect/token")
-				err := resourceMachineClientRead(d, m)
-				return []*schema.ResourceData{d}, err
-			},
-		},
+		CreateContext: resourceMachineClientCreate,
+		ReadContext:   resourceMachineClientRead,
+		DeleteContext: resourceMachineClientDelete,
+		UpdateContext: resourceMachineClientUpdate,
 		Schema: map[string]*schema.Schema{
 			"name": &schema.Schema{
 				Type:        schema.TypeString,
@@ -72,35 +63,70 @@ func resourceMachineClient() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"client_claims": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"claims": {
+							Type:     schema.TypeList,
+							Required: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 }
 
-func resourceMachineClientCreate(d *schema.ResourceData, m interface{}) error {
+func resourceMachineClientCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	machineClientInput := ReadMachineClientFromResourceData(d)
 	providerInput := m.(*ElvidProviderInput)
 	machineClient, err := elvidapiclient.CreateMachineClient(providerInput.ElvIDAuthority, providerInput.AccessTokenAD, machineClientInput)
 
+	// if err != nil {
+	// 	fmt.Printf("Error: %s\n", err)
+	// 	return err
+	// }
+
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return err
+		var diags diag.Diagnostics
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Creating machineclient resulted in an error",
+			Detail:   err.Error(),
+		})
+		return diags
 	}
 
 	d.SetId(strconv.Itoa(machineClient.Id))
 	d.Set("client_id", machineClient.ClientId)
 	d.Set("token_endpoint", providerInput.ElvIDAuthority+"/connect/token")
 
-	return resourceMachineClientRead(d, m)
+	return resourceMachineClientRead(ctx, d, m)
 }
 
-func resourceMachineClientRead(d *schema.ResourceData, m interface{}) error {
+func resourceMachineClientRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	providerInput := m.(*ElvidProviderInput)
 
 	machineClient, err := elvidapiclient.ReadMachineClient(providerInput.ElvIDAuthority, providerInput.AccessTokenAD, d.Id())
 
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return err
+		var diags diag.Diagnostics
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Reading machineclient resulted in an error",
+			Detail:   err.Error(),
+		})
+		return diags
 	}
 
 	if machineClient == nil {
@@ -120,7 +146,7 @@ func resourceMachineClientRead(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 
-func resourceMachineClientUpdate(d *schema.ResourceData, m interface{}) error {
+func resourceMachineClientUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	machineClientInput := ReadMachineClientFromResourceData(d)
 	machineClientInput.Id, _ = strconv.Atoi(d.Id())
 	providerInput := m.(*ElvidProviderInput)
@@ -128,22 +154,32 @@ func resourceMachineClientUpdate(d *schema.ResourceData, m interface{}) error {
 	_, err := elvidapiclient.UpdateMachineClient(providerInput.ElvIDAuthority, providerInput.AccessTokenAD, machineClientInput)
 
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return err
+		var diags diag.Diagnostics
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Updating machineclient resulted in an error",
+			Detail:   err.Error(),
+		})
+		return diags
 	}
 	d.Set("token_endpoint", providerInput.ElvIDAuthority+"/connect/token")
 
-	return resourceMachineClientRead(d, m)
+	return resourceMachineClientRead(ctx, d, m)
 }
 
-func resourceMachineClientDelete(d *schema.ResourceData, m interface{}) error {
+func resourceMachineClientDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	providerInput := m.(*ElvidProviderInput)
 
 	err := elvidapiclient.DeleteMachineClient(providerInput.ElvIDAuthority, providerInput.AccessTokenAD, d.Id())
 
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return err
+		var diags diag.Diagnostics
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Deleting machineclient resulted in an error",
+			Detail:   err.Error(),
+		})
+		return diags
 	}
 	return nil
 }
--- terraform-tester.tf
+++ terraform-tester.tf
@@ -6,21 +6,31 @@ provider "elvid" {
   terraform_sp_client_id = var.terraform_sp_client_id
   terraform_sp_client_secret = var.terraform_sp_client_secret
   environment = var.environment
-  # override_elvid_authority = "https://localhost:44383"
+  #override_elvid_authority = "https://localhost:44383"
   override_elvid_authority = "https://elvid.dev-elvia.io"
 }
+
+# provider "vault" {
+#   auth_login {
+#     path = "auth/approle/login"
+
+#     parameters = {
+#       role_id = var.elvid_dev_vault_role_id
+#     }
+#   }
+# }
 
 ## User client
 
 # resource "elvid_userclient" "userclient" {
 #     client_name = "test"
-#     scopes = ["louvre.imageapi.useraccess", "openid", "ad_groups"]
+#     scopes = ["terraform-provider-elvid-tester-apiscope2", "openid", "ad_groups"]
 #     domains = var.domains[var.environment]
 #     redirect_uri_paths = ["/callback.html"]
 #     post_logout_redirect_uri_paths = ["/index.htm"]
 #     bankid_login_enabled = true
 #     local_login_enabled = true
-#     elvia_ad_login_enabled = false
+#     elvia_ad_login_enabled = true
 #     test_user_login_enabled = false
 #     require_client_secret = false
 #     access_token_life_time = 3598
@@ -31,15 +41,26 @@ provider "elvid" {
 #     refresh_token_life_time = 2592000
 # }
 
+# output "userclient" {
+#   value = elvid_userclient.userclient
+# }
+
 ## Machine client
 
-# resource "elvid_machineclient" "machineclient" {
-#     name = "onsdag"
-#     test_user_login_enabled = true
-#     access_token_life_time = 3517
-#     resource_taint_version = "1"
-#     scopes = ["temp"]
-# }
+resource "elvid_machineclient" "machineclient" {
+    name = "onsdag"
+    test_user_login_enabled = true
+    access_token_life_time = 3517
+    resource_taint_version = "1"
+    scopes = ["elvid.verifydeployment"]
+    # client_claims = {"edna_topics_read" = ["topic1", "topic2"]}
+    # client_claims = {"edna_topics_read" = []}
+    client_claims {
+      name = "edna_topics_read"
+      claims = ["topic1"]
+    }
+    # client_claims = {"edna_topics_read" = "topic1"}
+}
 
 # resource "elvid_clientsecret" "clientsecret" {
 #     client_id = elvid_machineclient.machineclient.id
@@ -57,22 +78,24 @@ provider "elvid" {
 ## API scope
 
 # resource "elvid_apiscope" "apiscope" {
-#     name = "terraform-provider-elvid-tester-apiscope"
-#     description = "Scope opprettet fra test av Elvid Terraform provider (terraform-tester i terraform-provider-elvid)"
+#     name = "terraform-provider-elvid-tester-apiscope2"
+#     description = "3"
 #     user_claims = ["email", "ad_groups"]
-#     allow_machine_clients = true
+#     allow_user_clients = true
 # }
 
 ## Module userclient
 # module "elvid_userclient" {
 #   source      = "C:\\3lvia\\terraform-elvid-userclient"
 #   environment = "dev"
-#   client_name = "test-web"
+#   client_name = "test-bff"
 #   scopes = [ "louvre.imageapi.useraccess", "openid", "ad_groups"]
 #   domains = var.domains[var.environment]
 #   redirect_uri_paths = [ "/silentcallback.html", "/oidc/callback"]
 #   post_logout_redirect_uri_paths = [""]
 #   elvia_ad_login_enabled         = true
+#   system_name      = "elvid"
+#   client_secret_enabled = true
 # }
 
 ## Module machineclient
@@ -85,3 +108,33 @@ provider "elvid" {
 #   system_name      = "elvid"
 #   application_name = "demo-machineclient"
 # }
+
+variable "tenant_id" {
+}
+
+variable "terraform_sp_client_id" {
+}
+
+variable "terraform_sp_client_secret" {
+}
+
+variable "elvid_dev_vault_role_id" {
+}
+
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "system_name" {
+  default = "examplesystem"
+}
+
+variable "domains" {
+  type = map
+  default = {
+    "dev" = ["http://localhost:{port}", "https://examplesystem.dev-elvia.io"]
+    "test" = ["https://examplesystem.test-elvia.io"]
+    "prod" = ["https://examplesystem.elvia.io"]
+  }
+}
